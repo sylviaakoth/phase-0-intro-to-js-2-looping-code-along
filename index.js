1b9c40ca1ce0a31527c78703db043f4ca8417047
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-16 14:32:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(name) {
+    let cardNames = []
+    for (let index = 0; index < name.length; index++) {
+        cardNames.push(`Thank you, ${name[index]}, for the wonderful surprise gift!`);
+        } 
+    return cardNames;   
+}
+
+function countDown(number) {
+    while (number>=0) {
+        console.log(number); number--;
+    }
+}
